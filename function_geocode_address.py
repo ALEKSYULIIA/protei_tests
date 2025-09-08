9a38eb6230d8af9e7a6795bd238165a48f3aa1da
ALEKSYULIIA <mihjn@bk.ru>
2025-09-08 09:50:03 +0300

    changes

--- function_geocode_address.py
+++ function_geocode_address.py
@@ -1,30 +1,34 @@
 import requests
 
+BASE_URL_NOMINATIM = "https://nominatim.openstreetmap.org/search"
 
-def get_location_info_addr(address):
-    base_url_nominatim = "https://nominatim.openstreetmap.org/search"
+def get_location_info_addr(address, email = 'mihjn@bk.ru'):
     params = {
-    "q": address,
-    "format": "json",
-    "limit": 1,
-    }
+        "q": address,
+        "format": "json",
+        "limit": 1,
+        }
 
-    headers = {'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) \
-            Chrome/138.0.0.0 Safari/537.36 (mihjn@bk.ru)'
+    headers = {f'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) \
+            Chrome/138.0.0.0 Safari/537.36 ({email})'
     }
-    response = requests.get(base_url_nominatim, params=params, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        if data:
-            first_result = data[0]
-            return {
+    response = requests.get(BASE_URL_NOMINATIM, params=params, headers=headers)
+
+    if response.status_code != 200:
+        print(f"Error code: {response.status_code}")
+        print(f"Error reason: {response.reason}")
+        return {}
+
+    data = response.json()
+    if len(data) == 1:
+        first_result = data[0]
+        return {
                 "address": first_result.get("display_name"),
                 "lat": first_result.get("lat"),
                 "lon": first_result.get("lon"),
             }
     else:
-        print("Пустой список результатов")
-        print(f"Ошибка запроса: {response.status_code}")
+        print("More than one results")
 
 result = get_location_info_addr("Россия, Санкт-Петербург, Ипподромный переулок, 1 к1")
 print(result)
